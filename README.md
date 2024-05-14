01feca02bdad9f03dd27dc6ae7ed519aefaf6ed0
Lucas Dunn <dunnlu@classwork.engr.oregonstate.edu>
2024-05-14 15:28:15 -0700

    Initial app.js, htmlFiles (written by Chirstian Ritchie), readme, and package.json

---
+++ README.md
@@ -0,0 +1,21 @@
+### Dependencies
+
+To install dependencies run the following from main:
+
+    npm install
+
+### Execution
+
+To alias the forever command
+
+    alias forever='./node_modules/forever/bin/forever'
+
+To run the code
+
+    forever start app.js
+
+To stop the code
+
+    forever stop app.js
+
+Find the website [here](http://classwork.engr.oregonstate.edu:56294/)
---
+++ app.js
@@ -0,0 +1,36 @@
+// Express
+var express = require('express');   // We are using the express library for the web server
+var app     = express();            // We need to instantiate an express object to interact with the server in our code
+PORT        = 56294;                 // Set a port number at the top so it's easy to change in the future
+const path = require('path')
+
+
+app.get('/', function(req,res) {
+    console.log("Requested URL: ",req.url);
+    res.sendFile(path.join(__dirname, 'htmlFiles/index.html'));
+
+});
+
+
+app.get('/wishlists', function(req,res) {
+    console.log("Requested URL: ",req.url);
+    res.sendFile(path.join(__dirname, 'htmlFiles/wishlists.html'));
+
+});
+
+app.get('/deals', function(req,res){
+    console.log("Requested URL: ",req.url);
+    res.sendFile(path.join(__dirname, 'htmlFiles/deals.html'));
+    
+});
+
+app.get('/laptops', function(req,res) {
+    console.log("Requested URL: ",req.url);
+    res.sendFile(path.join(__dirname, 'htmlFiles/laptops.html'));
+
+});
+
+
+app.listen(PORT, function(){
+    console.log("Listening on port " + PORT + ".")
+});
