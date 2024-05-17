c276daac90ec8d1741a8d8d0d99ff12802970c9d
Lucas Dunn <dunnlu@classwork.engr.oregonstate.edu>
2024-05-16 17:37:39 -0700

    Added initial data query files

---
+++ dataQueries/DDL.sql
@@ -0,0 +1,192 @@
+-------------------------------------------------------------------------------
+-- Data Definition Queries
+-------------------------------------------------------------------------------
+--
+-- Authors:  		Team 136 - Lucas Dunn & Christian Ritchie
+-- Date:	  		2024-05-09
+-- Project:  		LaptopSpecsDB
+-- Phase:	  		Step 3, Draft
+--
+-------------------------------------------------------------------------------
+
+SET FOREIGN_KEY_CHECKS=0;
+SET AUTOCOMMIT = 0;
+
+-- MariaDB dump 10.19  Distrib 10.5.22-MariaDB, for Linux (x86_64)
+--
+-- Host: classmysql.engr.oregonstate.edu    Database: cs340_ritchchr
+-- ------------------------------------------------------
+-- Server version	10.6.17-MariaDB-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Deals`
+--
+
+DROP TABLE IF EXISTS `Deals`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Deals` (
+  `dealID` int(11) NOT NULL AUTO_INCREMENT,
+  `laptopID` int(11) DEFAULT NULL,
+  `storeID` int(11) DEFAULT NULL,
+  `timeStart` datetime DEFAULT NULL,
+  `timeEnd` datetime DEFAULT NULL,
+  `stock` varchar(8) DEFAULT NULL,
+  `price` decimal(19,2) DEFAULT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`dealID`),
+  KEY `laptopID` (`laptopID`),
+  KEY `storeID` (`storeID`),
+  CONSTRAINT `Deals_ibfk_1` FOREIGN KEY (`laptopID`) REFERENCES `Laptops` (`laptopID`),
+  CONSTRAINT `Deals_ibfk_2` FOREIGN KEY (`storeID`) REFERENCES `Stores` (`storeID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Deals`
+--
+
+LOCK TABLES `Deals` WRITE;
+/*!40000 ALTER TABLE `Deals` DISABLE KEYS */;
+INSERT INTO `Deals` VALUES (1,1,1,'2024-04-30 00:00:00',NULL,'high',2759.00,'https://www.hp.com/us-en/shop/mdp/hp-zbook-power-15-mobile-workstation'),(2,2,2,'2024-04-30 00:00:00',NULL,'low',1299.00,'https://www.qvc.com/msi-thin-gf63-156-144hz-gaming-laptop-i7-rtx40-60-16gb-512gb.product.E318491.html'),(3,3,2,'2024-04-30 00:00:00',NULL,'low',1999.98,'https://www.omen.com/us/en/laptops/2019-omen-15/specifications.html');
+/*!40000 ALTER TABLE `Deals` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Laptops`
+--
+
+DROP TABLE IF EXISTS `Laptops`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Laptops` (
+  `laptopID` int(11) NOT NULL AUTO_INCREMENT,
+  `laptopName` varchar(50) DEFAULT NULL,
+  `brandName` varchar(50) DEFAULT NULL,
+  `gpu` varchar(50) DEFAULT NULL,
+  `cpu` varchar(50) DEFAULT NULL,
+  `ram` int(11) DEFAULT NULL,
+  `internalStorage` int(11) DEFAULT NULL,
+  `displaySize` int(11) DEFAULT NULL,
+  PRIMARY KEY (`laptopID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Laptops`
+--
+
+LOCK TABLES `Laptops` WRITE;
+/*!40000 ALTER TABLE `Laptops` DISABLE KEYS */;
+INSERT INTO `Laptops` VALUES (1,'Zbook','HP','Nvidia RTX 2000',' Intel i7 5.2 GHz',64,2,16),(2,'Thin GF63','MSI','NVIDIA GeForce GTX 1650 ',' Intel i7 4.7 GHz',16,512,16),(3,'Omen','HP','NVIDIA GeForce RTX 2060',' Intel i7 5 GHz',16,512,17);
+/*!40000 ALTER TABLE `Laptops` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Results` (intersection table between Specs:Deals)
+--
+
+DROP TABLE IF EXISTS `Results`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Results` (
+  `specsID` int(11) NOT NULL,
+  `dealID` int(11) NOT NULL,
+  PRIMARY KEY (`specsID`,`dealID`),
+  KEY `dealID` (`dealID`),
+  CONSTRAINT `Results_ibfk_1` FOREIGN KEY (`specsID`) REFERENCES `Specs` (`specsID`) ON DELETE CASCADE,
+  CONSTRAINT `Results_ibfk_2` FOREIGN KEY (`dealID`) REFERENCES `Deals` (`dealID`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Results`
+--
+
+LOCK TABLES `Results` WRITE;
+/*!40000 ALTER TABLE `Results` DISABLE KEYS */;
+INSERT INTO `Results` VALUES (1,1),(1,2),(2,3);
+/*!40000 ALTER TABLE `Results` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Specs`
+--
+
+DROP TABLE IF EXISTS `Specs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Specs` (
+  `specsID` int(11) NOT NULL AUTO_INCREMENT,
+  `specsName` varchar(50) DEFAULT NULL,
+  `brandName` varchar(50) DEFAULT NULL,
+  `gpu` varchar(50) DEFAULT NULL,
+  `cpu` varchar(50) DEFAULT NULL,
+  `ram` int(11) DEFAULT NULL,
+  `internalStorage` int(11) DEFAULT NULL,
+  `displaySize` int(11) DEFAULT NULL,
+  `budget` decimal(19,2) DEFAULT NULL,
+  PRIMARY KEY (`specsID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Specs`
+--
+
+LOCK TABLES `Specs` WRITE;
+/*!40000 ALTER TABLE `Specs` DISABLE KEYS */;
+INSERT INTO `Specs` VALUES (1,'Joe\'s List',NULL,'NVIDIA GeForce RTX 2060','0',16,512,14,3000.00),(2,'Mariana\'s List','H',NULL,'Intel i7 5 GHz',16,128,16,2000.00);
+/*!40000 ALTER TABLE `Specs` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Stores`
+--
+
+DROP TABLE IF EXISTS `Stores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Stores` (
+  `storeID` int(11) NOT NULL AUTO_INCREMENT,
+  `storeName` varchar(50) DEFAULT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`storeID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Stores`
+--
+
+LOCK TABLES `Stores` WRITE;
+/*!40000 ALTER TABLE `Stores` DISABLE KEYS */;
+INSERT INTO `Stores` VALUES (1,'HP','https://www.hp.com/us-en/home.html'),(2,'QVC','https://www.qvc.com/');
+/*!40000 ALTER TABLE `Stores` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-05-09 18:32:19
+
+
+SET FOREIGN_KEY_CHECKS=1;
+COMMIT;
---
+++ dataQueries/DML.sql
@@ -0,0 +1,185 @@
+-------------------------------------------------------------------------------
+-- Data Manipulation Queries
+-------------------------------------------------------------------------------
+--
+-- Authors:  		Team 136 - Lucas Dunn & Christian Ritchie
+-- Date:	  		2024-05-09
+-- Project:  		LaptopSpecsDB
+-- Phase:	  		Step 3, Draft
+--
+-------------------------------------------------------------------------------
+------ Spec wishlist queries
+-------------------------------------------------------------------------------
+
+-- get all spec wishlist spec IDs, names, and information to populate main page
+SELECT * FROM Specs
+
+-- add a new spec wishlist
+INSERT INTO Specs (specsName, brandName, gpu, cpu, ram, internalStorage, displaySize, budget) 
+VALUES (
+	:specsNameInput, 
+	:brandNameInput, 
+	:gpuInput, 
+	:cpuInput, 
+	:ramInput, 
+	:internalStorageInput,
+	:displaySizeInput, 
+	:budgetInput
+	)
+
+-- delete a spec wishlist
+DELETE FROM Specs WHERE specsID = :spec_ID_selected_from_spec_list
+
+-- update a spec wishlist
+UPDATE Specs 
+SET 
+	specsName = :specsNameInput,
+	brandName= :brandNameInput,
+	gpu = :gpuInput,
+	cpu = :cpuInput,
+	ram = :ramInput,
+	internalStorage = :internalStorageInput,
+	displaySize = :displaySizeInput,
+	budget = :budgetInput
+WHERE specsID= :specsID_from_update_form
+
+-------------------------------------------------------------------------------
+------ Laptop queries
+-------------------------------------------------------------------------------
+
+-- add a new laptop
+INSERT INTO Laptops (laptopName, brandName, gpu, cpu, ram, internalStorage, displaySize) 
+VALUES (
+	:laptopNameInput, 
+	:brandNameInput, 
+	:gpuInput, 
+	:cpuInput, 
+	:ramInput, 
+	:internalStorageInput, 
+	:displaySizeInput
+	)
+
+-- remove a laptop
+DELETE FROM Laptops WHERE laptopID = :laptop_ID_selected_from_laptop_list
+
+-- update a laptop
+UPDATE Laptops 
+SET 
+	laptopName = :laptopNameInput, 
+	brandName = :brandNameInput, 
+	gpu = :gpuInput, 
+	cpu = :cpuInput, 
+	ram = :ramInput, 
+	internalStorage = :internalStorageInput, 
+	displaySize = :displaySizeInput 
+WHERE 
+	laptopID = :laptopID_from_the_update_form
+
+-------------------------------------------------------------------------------
+------ Deal queries (M-to-M associations facilitated by 'Results' table)
+-------------------------------------------------------------------------------
+
+-- add a deal
+INSERT INTO Deals (laptopID, storeID, timeStart, timeEnd, stock, price, url)
+VALUES (
+    (SELECT laptopID FROM Laptops WHERE laptopName = :laptopNameInput),
+    (SELECT storeID FROM Stores WHERE storeName = :storeNameInput),
+    :timeStart,
+    :timeEnd,
+    :stock,
+    :price,
+    :url
+)
+
+-- retrieve deals associated with a wishlist to display on a wishlist's page
+SELECT * FROM Deals WHERE dealID = (
+	SELECT dealID FROM Results WHERE specsID = :SpecsIDInput
+	) 
+	AND (timeEnd > NOW() OR timeEnd IS NULL)
+
+
+------ Deals queries -- selective association and dis-association (M-to-M)
+
+
+-- populate 'results' intersection table with deals given a specific deal
+-- LIKE concat(); https://www.tutorialspoint.com/can-we-use-like-concat-in-a-mysql-query
+INSERT INTO Results (specsID, dealID)
+SELECT Specs.specsID, Deals.dealID FROM Specs
+INNER JOIN Laptops ON (
+    (Specs.brandName IS NULL OR Laptops.brandName LIKE CONCAT('%', Specs.brandName, '%'))
+    AND (Specs.gpu IS NULL OR Laptops.gpu LIKE CONCAT('%', Specs.gpu, '%'))
+    AND (Specs.cpu IS NULL OR Laptops.cpu LIKE CONCAT('%', Specs.cpu, '%'))
+    AND (Specs.ram IS NULL OR Laptops.ram >= Specs.ram)
+    AND (Specs.internalStorage IS NULL OR Laptops.internalStorage >= Specs.internalStorage)
+    AND (Specs.displaySize IS NULL OR Laptops.displaySize = Specs.displaySize)
+	)
+INNER JOIN Deals ON Laptops.laptopID = Deals.laptopID
+WHERE Specs.specsID = :specID
+	AND (Deals.price IS NULL OR Specs.budget >= Deals.price)
+
+-- remove expired deals from results (should occur before displaying deals)
+-- NOW() function	https://www.w3schools.com/sql/func_mysql_now.asp
+DELETE FROM Results WHERE dealID IN (
+    SELECT dealID FROM Deals WHERE timeEnd IS NOT NULL 
+		AND timeEnd < NOW()
+	)
+
+
+------ Deals queries -- manutal association and dis-association (M-to-M)
+
+
+-- associate a spec wishlist with a deal (M-to-M association)
+INSERT INTO Results (specsID, dealID) VALUES (:specsID, :dealID)
+
+-- dis-associate a spec wishlist from a deal (M-to-M dis-association)
+DELETE FROM Results WHERE specsID = :specsID AND dealID = :dealID
+
+-------------------------------------------------------------------------------
+------ Store queries
+-------------------------------------------------------------------------------
+
+-- retrieve all stores
+SELECT * FROM Stores
+
+-- add store
+INSERT INTO Stores (storeName, url) VALUES (:storeNameInput, :urlInput)
+
+-- delete store
+DELETE FROM Stores WHERE storeID = :storeID
+
+-------------------------------------------------------------------------------
+------ potentially useful unused queries and subqueries
+-------------------------------------------------------------------------------
+
+-- populate 'results' intersection table; associating all spec wishlists with
+-- laptops with attributes that have a partial match to each specs attribute.
+INSERT INTO Results (specsID, laptopID)
+SELECT Specs.specsID, Laptops.laptopID FROM Specs
+INNER JOIN Laptops ON (
+    (Specs.brandName IS NULL OR Laptops.brandName LIKE CONCAT('%', Specs.brandName, '%'))
+    AND (Specs.gpu IS NULL OR Laptops.gpu LIKE CONCAT('%', Specs.gpu, '%'))
+    AND (Specs.cpu IS NULL OR Laptops.cpu LIKE CONCAT('%', Specs.cpu, '%'))
+    AND (Specs.ram IS NULL OR Laptops.ram >= Specs.ram)
+    AND (Specs.internalStorage IS NULL OR Laptops.internalStorage >= Specs.internalStorage)
+    AND (Specs.displaySize IS NULL OR Laptops.displaySize = Specs.displaySize)
+)
+
+-- get laptopIDs correlated with a spec from the Results intersection table
+SELECT Laptops.laptopID
+FROM Specs
+INNER JOIN Laptops ON (
+    (Specs.brandName IS NULL OR Laptops.brandName LIKE CONCAT('%', Specs.brandName, '%'))
+    AND (Specs.gpu IS NULL OR Laptops.gpu LIKE CONCAT('%', Specs.gpu, '%'))
+    AND (Specs.cpu IS NULL OR Laptops.cpu LIKE CONCAT('%', Specs.cpu, '%'))
+    AND (Specs.ram IS NULL OR Laptops.ram >= Specs.ram)
+    AND (Specs.internalStorage IS NULL OR Laptops.internalStorage >= Specs.internalStorage)
+    AND (Specs.displaySize IS NULL OR Laptops.displaySize = Specs.displaySize)
+)
+WHERE Specs.specsID = :specID
+
+-- get deals associated with a laptopID
+SELECT *
+FROM Deals
+WHERE laptopID = :laptopIDInput
+	AND (Deals.price IS NULL OR Specs.budget >= Deals.price)
+
