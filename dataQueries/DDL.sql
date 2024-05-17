c494e79d3fdb2d6fb43881182c369e08f49e5285
cfritchie <cfritchie.hi@gmail.com>
2024-05-16 17:41:56 -1000

    added additional example data to DDL.sql

--- dataQueries/DDL.sql
+++ dataQueries/DDL.sql
@@ -59,7 +59,11 @@ CREATE TABLE `Deals` (
 
 LOCK TABLES `Deals` WRITE;
 /*!40000 ALTER TABLE `Deals` DISABLE KEYS */;
-INSERT INTO `Deals` VALUES (1,1,1,'2024-04-30 00:00:00',NULL,'high',2759.00,'https://www.hp.com/us-en/shop/mdp/hp-zbook-power-15-mobile-workstation'),(2,2,2,'2024-04-30 00:00:00',NULL,'low',1299.00,'https://www.qvc.com/msi-thin-gf63-156-144hz-gaming-laptop-i7-rtx40-60-16gb-512gb.product.E318491.html'),(3,3,2,'2024-04-30 00:00:00',NULL,'low',1999.98,'https://www.omen.com/us/en/laptops/2019-omen-15/specifications.html');
+INSERT INTO `Deals` VALUES 
+(1,1,1,'2024-04-30 00:00:00',NULL,'high',2759.00,'https://www.hp.com/us-en/shop/mdp/hp-zbook-power-15-mobile-workstation'),
+(2,2,2,'2024-04-30 00:00:00',NULL,'low',1299.00,'https://www.qvc.com/msi-thin-gf63-156-144hz-gaming-laptop-i7-rtx40-60-16gb-512gb.product.E318491.html'),
+(3,3,2,'2024-04-30 00:00:00',NULL,'low',1999.98,'https://www.omen.com/us/en/laptops/2019-omen-15/specifications.html'),
+(4,4,3,'2024-05-16 00:00:00',NULL,NULL,1849.99,'https://www.newegg.com/blue-msi-ge-series-ge76-raider-11ue-046-gaming/p/2WC-000C-0DDJ4?Item=9SIADG3J7R0690&cm_sp=SP-_-1798083-_-0-_-2-_-9SIADG3J7R0690-_-MSI%20Nvidia%20GeForce-_-msi-_-2');
 /*!40000 ALTER TABLE `Deals` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -89,7 +93,11 @@ CREATE TABLE `Laptops` (
 
 LOCK TABLES `Laptops` WRITE;
 /*!40000 ALTER TABLE `Laptops` DISABLE KEYS */;
-INSERT INTO `Laptops` VALUES (1,'Zbook','HP','Nvidia RTX 2000',' Intel i7 5.2 GHz',64,2,16),(2,'Thin GF63','MSI','NVIDIA GeForce GTX 1650 ',' Intel i7 4.7 GHz',16,512,16),(3,'Omen','HP','NVIDIA GeForce RTX 2060',' Intel i7 5 GHz',16,512,17);
+INSERT INTO `Laptops` VALUES 
+  (1,'Zbook','HP','Nvidia RTX 2000','Intel i7 5.2 GHz',64,2,16),
+  (2,'Thin GF63','MSI','NVIDIA GeForce GTX 1650 ','Intel i7 4.7 GHz',16,512,16),
+  (3,'Omen','HP','NVIDIA GeForce RTX 2060','Intel i7 5 GHz',16,512,17),
+  (4,'GE76 Raider','MSI','NVIDIA GeForce RTX 3060','Intel i7 2.3 GHz',32,1,16);
 /*!40000 ALTER TABLE `Laptops` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -116,7 +124,7 @@ CREATE TABLE `Results` (
 
 LOCK TABLES `Results` WRITE;
 /*!40000 ALTER TABLE `Results` DISABLE KEYS */;
-INSERT INTO `Results` VALUES (1,1),(1,2),(2,3);
+INSERT INTO `Results` VALUES (1,1),(1,2),(2,3),(3,4);
 /*!40000 ALTER TABLE `Results` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -147,7 +155,10 @@ CREATE TABLE `Specs` (
 
 LOCK TABLES `Specs` WRITE;
 /*!40000 ALTER TABLE `Specs` DISABLE KEYS */;
-INSERT INTO `Specs` VALUES (1,'Joe\'s List',NULL,'NVIDIA GeForce RTX 2060','0',16,512,14,3000.00),(2,'Mariana\'s List','H',NULL,'Intel i7 5 GHz',16,128,16,2000.00);
+INSERT INTO `Specs` VALUES 
+  (1,'Joe\'s List',NULL,'NVIDIA GeForce RTX 2060','0',16,512,14,3000.00),
+  (2,'Mariana\'s List','H',NULL,'Intel i7 5 GHz',16,128,16,2000.00),
+  (3,'Schindler''s list',NULL,'Nvidia Geforce RTX',NULL,32,NULL,16,1900.00);
 /*!40000 ALTER TABLE `Specs` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -172,7 +183,8 @@ CREATE TABLE `Stores` (
 
 LOCK TABLES `Stores` WRITE;
 /*!40000 ALTER TABLE `Stores` DISABLE KEYS */;
-INSERT INTO `Stores` VALUES (1,'HP','https://www.hp.com/us-en/home.html'),(2,'QVC','https://www.qvc.com/');
+INSERT INTO `Stores` VALUES 
+(1,'HP','https://www.hp.com/us-en/home.html'),(2,'QVC','https://www.qvc.com/'),(3,'NewEgg','https://www.newegg.com/');
 /*!40000 ALTER TABLE `Stores` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
