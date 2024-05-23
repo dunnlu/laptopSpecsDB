74c12ac4fcefe374cc9598b9f49b8c0e313c49f8
Lucas Dunn <dunnlu@classwork.engr.oregonstate.edu>
2024-05-23 16:55:56 -0700

    Converted stores to handlebars file, uploaded data from actual database

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ var app     = express();            // We need to instantiate an express object 
 PORT        = 46737;                // Set a port number at the top so it's easy to change in the future
 const path = require('path')
 
+//Database
+var db = require('./dataQueries/db-connector')
+
+
+// Handlebars
+const { engine } = require('express-handlebars');
+var exphbs = require('express-handlebars');     // Import express-handlebars
+app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
+app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
+
 
 app.get('/', function(req,res) {
     console.log("Requested URL: ",req.url);
@@ -37,9 +47,12 @@ app.get('/results', function(req,res) {
 });
 
 app.get('/stores', function(req,res) {
-    console.log("Requested URL: ",req.url);
-    res.sendFile(path.join(__dirname, 'htmlFiles/stores.html'));
+    let query1 = "SELECT * FROM Stores;";               // Define our query
 
+    db.pool.query(query1, function(error, rows, fields){    // Execute the query
+
+        res.render('stores', {data: rows});                  // Render the index.hbs file, and also send the renderer
+    })  
 });
 
 
