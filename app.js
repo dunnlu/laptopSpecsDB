b3c30d7595850c35a96bcfe21c7bbdd9fd20ad54
Lucas Dunn <dunnlu@classwork.engr.oregonstate.edu>
2024-05-30 19:22:44 -0700

    Added CRUD for Specs

--- app.js
+++ app.js
@@ -27,10 +27,296 @@ app.get('/', function(req,res) {
 
 
 app.get('/specs', function(req,res) {
-    console.log("Requested URL: ",req.url);
-    res.sendFile(path.join(__dirname, 'htmlFiles/specs.html'));
+    let query1 = "SELECT * FROM Specs;";               // Define our query
+
+    db.pool.query(query1, function(error, rows, fields){    // Execute the query
+
+        res.render('specs', {data: rows});                  // Render the index.hbs file, and also send the renderer
+    })  
 
 });
+
+app.post('/addSpec', function(req, res) 
+{
+    // Capture the incoming data and parse it back to a JS object
+    let data = req.body;
+
+    // Capture NULL values
+    let specsName = data['input-specsName'];
+    if (specsName==='')
+    {
+        specsName = 'NULL'
+    }
+
+    let brandName = data['input-brandName'];
+    if (brandName==='')
+    {
+        brandName = 'NULL'
+    }
+
+    let gpu = data['input-gpu'];
+    if (gpu==='')
+    {
+        gpu = 'NULL'
+    }
+
+    let cpu = data['input-cpu'];
+    if (cpu==='')
+    {
+        cpu = 'NULL'
+    }
+
+    let ram = data['input-ram'];
+    if (ram==='')
+    {
+        ram = 'NULL'
+    }
+
+    let internalStorage = data['input-internalStorage'];
+    if (internalStorage==='')
+    {
+        internalStorage = 'NULL'
+    }
+
+    let displaySize = data['input-displaySize'];
+    if (displaySize==='')
+    {
+        displaySize = 'NULL'
+    }
+
+    let budget = data['input-budget'];
+    if (budget==='')
+    {
+        budget = 'NULL'
+    }
+
+
+
+
+    // Create the query and run it on the database
+    query1 = `INSERT INTO Specs (specsName, brandName, gpu, cpu, ram, internalStorage, displaySize, budget) 
+    VALUES (
+        "${specsName}", 
+        "${brandName}", 
+        "${gpu}", 
+        "${cpu}", 
+        "${ram}", 
+        "${internalStorage}",
+        "${displaySize}", 
+        "${budget}"
+        )`;
+    db.pool.query(query1, function(error, rows, fields){
+
+        // Check to see if there was an error
+        if (error) {
+
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error)
+            res.sendStatus(400);
+        }
+        else
+        {
+            // If there was no error, perform a SELECT * on bsg_people
+            query2 = `SELECT * FROM Specs;`;
+            db.pool.query(query2, function(error, rows, fields){
+
+                // If there was an error on the second query, send a 400
+                if (error) {
+                    
+                    // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                    console.log(error);
+                    res.sendStatus(400);
+                }
+                // If all went well, send the results of the query back.
+                else
+                {
+                    console.log("redirecting")
+                    res.redirect('/specs')
+                }
+            })
+        }
+    })
+});
+
+
+app.post('/deleteSpec', function(req, res) 
+{
+    // Capture the incoming data and parse it back to a JS object
+    let data = req.body;
+
+
+    // Create the query and run it on the database
+    query1 = `DELETE FROM Specs WHERE specsID = ${data['delete-id']}`;
+    db.pool.query(query1, function(error, rows, fields){
+
+        // Check to see if there was an error
+        if (error) {
+
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error)
+            res.sendStatus(400);
+        }
+        else
+        {
+            // If there was no error, perform a SELECT * on bsg_people
+            query2 = `SELECT * FROM Specs;`;
+            db.pool.query(query2, function(error, rows, fields){
+
+                // If there was an error on the second query, send a 400
+                if (error) {
+                    
+                    // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                    console.log(error);
+                    res.sendStatus(400);
+                }
+                // If all went well, send the results of the query back.
+                else
+                {
+                    console.log("redirecting")
+                    res.redirect('/specs')
+                }
+            })
+        }
+    })
+});
+
+
+app.post('/updateSpec', function(req, res) 
+{
+    // Capture the incoming data and parse it back to a JS object
+    let data = req.body;
+
+
+
+    let updateString = ``;
+    let notFirst = false;
+
+    // Capture NULL values
+
+    id = data['update-id'];
+
+    let specsName = data['update-specsName'];
+    if (specsName!=='')
+    {
+        updateString = `specsName = "${specsName}"`;
+        notFirst=true;
+    }
+
+    let brandName = data['update-brandName'];
+    if (brandName!=='')
+    {
+        if (notFirst) {
+            updateString+=`, `;
+        }
+        updateString+=`brandName = "${brandName}"`;
+        notFirst = true;
+    }
+
+    let gpu = data['update-gpu'];
+    if (gpu!=='')
+    {
+        if (notFirst) {
+            updateString+=`, `;
+        }
+        updateString+=`gpu = "${gpu}"`;
+        notFirst = true;
+    }
+
+    let cpu = data['update-cpu'];
+    if (cpu!=='')
+    {
+        if (notFirst) {
+            updateString+=`, `;
+        }
+        updateString+=`cpu = "${cpu}"`;
+        notFirst = true;
+    }
+
+    let ram = data['update-ram'];
+    if (ram!=='')
+    {
+        if (notFirst) {
+            updateString+=`, `;
+        }
+        updateString+=`ram = "${ram}"`;
+        notFirst = true;
+    }
+
+    let internalStorage = data['update-internalStorage'];
+    if (internalStorage!=='')
+    {
+        if (notFirst) {
+            updateString+=`, `;
+        }
+        updateString+=`internalStorage = "${internalStorage}"`;
+        notFirst = true;
+    }
+
+    let displaySize = data['update-displaySize'];
+    if (displaySize!=='')
+    {
+        if (notFirst) {
+            updateString+=`, `;
+        }
+        updateString+=`displaySize = "${displaySize}"`;
+        notFirst = true;
+    }
+
+    let budget = data['update-budget'];
+    if (budget!=='')
+    {
+        if (notFirst) {
+            updateString+=`, `;
+        }
+        updateString+=`budget = "${budget}"`;
+        notFirst = true;
+    }
+
+    if (notFirst) {
+        // Create the query and run it on the database
+        query1 = `UPDATE Specs 
+        SET 
+            ${updateString}
+        WHERE specsID= ${id}`;
+        db.pool.query(query1, function(error, rows, fields){
+
+            // Check to see if there was an error
+            if (error) {
+
+                // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                console.log(error)
+                res.sendStatus(400);
+            }
+            else
+            {
+                // If there was no error, perform a SELECT * on bsg_people
+                query2 = `SELECT * FROM Specs;`;
+                db.pool.query(query2, function(error, rows, fields){
+
+                    // If there was an error on the second query, send a 400
+                    if (error) {
+                        
+                        // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                        console.log(error);
+                        res.sendStatus(400);
+                    }
+                    // If all went well, send the results of the query back.
+                    else
+                    {
+                        console.log("redirecting")
+                        res.redirect('/specs')
+                    }
+                })
+            }
+        })
+    } else {
+        res.redirect('/specs')
+    }
+
+
+    
+});
+
 
 app.get('/deals', function(req,res){
     console.log("Requested URL: ",req.url);
@@ -158,55 +444,69 @@ app.post('/updateStore', function(req, res)
     // Capture the incoming data and parse it back to a JS object
     let data = req.body;
 
+    let updateString = ``;
+    let notFirst = false;
+
+
 
     id = data['update-id'];
     
     let storeName = data['update-storeName'];
-    if (storeName==='')
+    if (storeName!=='')
     {
-        storeName = 'NULL'
-    }
+        updateString+= `storeName = "${storeName}"`;
+        notFirst = true;
+    } 
 
     let url = data['update-url'];
-    if (url==='')
+    if (url!=='')
     {
-        url = 'NULL'
+        if (notFirst) {
+            updateString+=', ';
+        }
+        updateString+=`url = "${url}"`;
     }
 
 
-    // Create the query and run it on the database
-    query1 = `UPDATE Stores SET storeName = "${storeName}", url = "${url}" WHERE storeID = ${id};`;
-    db.pool.query(query1, function(error, rows, fields){
 
-        // Check to see if there was an error
-        if (error) {
+    if (notFirst) {
 
-            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
-            console.log(error)
-            res.sendStatus(400);
-        }
-        else
-        {
-            // If there was no error, perform a SELECT * on bsg_people
-            query2 = `SELECT * FROM Stores;`;
-            db.pool.query(query2, function(error, rows, fields){
+        // Create the query and run it on the database
+        query1 = `UPDATE Stores SET ${updateString} WHERE storeID = ${id};`;
+        db.pool.query(query1, function(error, rows, fields){
 
-                // If there was an error on the second query, send a 400
-                if (error) {
-                    
-                    // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
-                    console.log(error);
-                    res.sendStatus(400);
-                }
-                // If all went well, send the results of the query back.
-                else
-                {
-                    console.log("redirecting")
-                    res.redirect('/stores')
-                }
-            })
-        }
-    })
+            // Check to see if there was an error
+            if (error) {
+
+                // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                console.log(error)
+                res.sendStatus(400);
+            }
+            else
+            {
+                // If there was no error, perform a SELECT * on bsg_people
+                query2 = `SELECT * FROM Stores;`;
+                db.pool.query(query2, function(error, rows, fields){
+
+                    // If there was an error on the second query, send a 400
+                    if (error) {
+                        
+                        // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                        console.log(error);
+                        res.sendStatus(400);
+                    }
+                    // If all went well, send the results of the query back.
+                    else
+                    {
+                        console.log("redirecting")
+                        res.redirect('/stores')
+                    }
+                })
+            }
+        })
+    } else {
+        res.redirect('/stores')
+    }
 });
 
 app.listen(PORT, function(){
